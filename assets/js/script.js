c27bb70fbb74afc1d79953546fbc43bb77790176
jlave2 <jlave2@uic.edu>
2016-09-08 12:07:18 -0500

    Got d3 working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,35 @@
-var s = d3.select('svg');
+let s = d3.select('svg');
+let w = s.attr('width');
+let h = s.attr('height');
 
-console.log(s);
+console.log(w, h);
 
-s.attr('height', 800);
-s.attr('width', 800);
+var data = null;
 
-s.append('circle') 
-    .attr('r', 25)
-    .attr('cx', 50)
-    .attr('cy', 200)
-    .style('fill', 'red');
+d3.json('/data.json', function(d) {
+    data = d;
+    bindData();
+});
+
+function bindData() {
+    s.selectAll('circle')
+        .data(data)
+        .enter()
+        .append('circle') 
+        .attr({
+            'r':Math.floor((Math.random() * 50) + 1),
+            'cx':function(d) {return d.x;},
+            'cy':function(d) {return d.y;}})
+        .style('fill', 'teal');
+}
+
+
+/*for (let j=1; j<=3; j++) {
+    for(let i=1; i<=7; i++) {
+        s.append('circle') 
+            .attr('r', Math.floor((Math.random() * 50) + 1))
+            .attr('cx', i * w / 8)
+            .attr('cy', j * h / 4)
+            .style('fill', 'teal');
+    }
+}*/
